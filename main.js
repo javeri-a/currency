68006f4c4b48168e4c6b820e4a4faebfb340630d
javeri-a <160253455+javeri-a@users.noreply.github.com>
2024-04-07 00:37:09 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1, // base currency
+    EUR: 0.92,
+    GBP: 0.79,
+    INR: 83.30,
+    PKR: 278,
+    ZAR: 18.73,
+    YUAN: 7.23,
+    AUD: 1.52,
+    BRL: 5.07,
+    CAD: 1.36
+};
+let currencyAnswer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter From Currency:",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "ZAR", "YUAN", "AUD", "BRL", "CAD"],
+    },
+    {
+        name: "to",
+        message: "Enter To Currency:",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "ZAR", "YUAN", "AUD", "BRL", "CAD"],
+    },
+    {
+        name: "amount",
+        message: "Enter Your Amount:",
+        type: "number",
+    },
+]);
+// console.log(currencyAnswer);
+let fromAmount = currency[currencyAnswer.from]; // exchange rate
+let toAmount = currency[currencyAnswer.to]; // exchange rate
+let amount = currencyAnswer.amount;
+let baseAmount = amount / fromAmount; // USD base currency
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
+// console.log(fromAmount);
+// console.log(toAmount);
+// console.log(amount);
+
+
+
